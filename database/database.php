bba19cd2f027d9f75f340a5e27984801d7fa3e62
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-03-02 15:44:10 +0100

    prova struttura

---
+++ database/database.php
@@ -0,0 +1,34 @@
+<?php
+//qui va scritto tutto il database  l'array ecc
+$albums=[
+    [
+        "images"=>"",
+        "title"=>"New Jersi",
+        "artist"=>"Bon Jovi",
+        "date"=>"1988"
+    ],
+    [
+        "images"=>"",
+        "title"=>"Live at Wembly 86",
+        "artist"=>"Queen",
+        "date"=>"1992"
+    ],    [
+        "images"=>"",
+        "title"=>"Ten's Summoner's Tales",
+        "artist"=>"Sting",
+        "date"=>"1993"
+    ],    [
+        "images"=>"",
+        "title"=>"Steve Gadd Badd",
+        "artist"=>"Steve Gadd Badd",
+        "date"=>"2018"
+    ],    [
+        "images"=>"",
+        "title"=>"Brave new world",
+        "artist"=>"Iron Maiden",
+        "date"=>"2000"
+    ],
+];
+
+
+?>
