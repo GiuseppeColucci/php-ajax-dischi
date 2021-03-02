05d511c69d41a65937a4baabd3bd1e3741e7c0f2
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-03-02 17:06:31 +0100

    prova 3 server

--- app/server.php
+++ app/server.php
@@ -1,7 +1,7 @@
 
 <?php
 header('Content-Type: application/json');
+require_once __DIR__ . '/database/database.php';
  echo json_encode($albums);
- require_once __DIR__ . '/database/database.php';
 
 ?>
